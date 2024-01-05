7a086f7ec2fc1084fc3588b5584abe7058578714
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-01-06 00:31:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/uretkenakademip3/",
   server: {
     port: 5500, // Bu portu istediğiniz portla değiştirebilirsiniz
   },
